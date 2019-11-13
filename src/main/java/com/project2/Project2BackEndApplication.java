a9b0bde7cd93a548608644cab55f06fcb47a0ef7
randolphbailey <randolphbailey@gmail.com>
2019-11-13 12:39:44 -0600

    init

---
+++ src/main/java/com/project2/Project2BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2BackEndApplication.class, args);
+	}
+
+}
